2c0bcf5a38738842ac2e09a8666144171bf18508
hugosenari <hugosenari@gmail.com>
2022-03-12 04:30:08 -0300

    chore: re index nimosc

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A wrapper around liblo for the Open Sound Control (OSC) protocol'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimosc-main".url = "path:./main";
+  inputs."nimosc-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimosc-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A wrapper around liblo for the Open Sound Control (OSC) protocol'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimosc-main.flake = false;
+  inputs.src-nimosc-main.owner = "Psirus";
+  inputs.src-nimosc-main.ref   = "refs/heads/main";
+  inputs.src-nimosc-main.repo  = "NimOSC";
+  inputs.src-nimosc-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimosc-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimosc-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
